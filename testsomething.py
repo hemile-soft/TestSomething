a2d99063329a424a1da80a7c6231b8b3edbdbe5b
hemile-soft <70147762+hemile-soft@users.noreply.github.com>
2024-11-21 10:35:49 +0100

    Create testsomething.py

---
+++ testsomething.py
@@ -0,0 +1,23 @@
+
+
+  def funny_function(para: str):
+    """
+    Returns something funny.
+  
+    :param para: Some string value.
+    :return: Another string value.
+    """
+    return f"{para}... how funny!"
+
+
+def another_function(a, b, c):
+    """
+    Print parameters in the terminal.
+  
+    :param a: Value 1
+    :param b: Value 2
+    :param c: Value 3
+    """
+    print(a)
+    print(b)
+    print(c)
