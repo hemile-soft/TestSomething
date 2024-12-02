99ebb81dd3ca7109179c56b05b4365cc5cfe9e35
hemile-soft <70147762+hemile-soft@users.noreply.github.com>
2024-12-02 15:36:42 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ import sys
 
 from sphinx.ext import autodoc
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 #sys.path.insert(0, str(Path('../../', '.').resolve()))
 
