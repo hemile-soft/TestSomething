57ae89167649c475c6c5638761e2e67aaee8dc9f
hemile-soft <70147762+hemile-soft@users.noreply.github.com>
2024-12-02 15:43:01 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,6 +38,7 @@ extensions = [
   'sphinx.ext.autodoc',
 ]
 
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
